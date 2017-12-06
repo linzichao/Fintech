68c7eb775550f9b905a5c16524220150c543da33
linzichao <joy10393@gmail.com>
2017-12-06 20:03:29 +0800

    Modify

--- index.js
+++ index.js
@@ -63,20 +63,54 @@ app.get('/webhook', (req, res) => {
 		}
 });
 
-const { MessengerBot } = require('bottender');
-const { createServer } = require('bottender/express');
+// const { MessengerBot } = require('bottender');
+// const { createServer } = require('bottender/express');
 
-const bot = new MessengerBot({
-  accessToken: 'EAAYzaWcruX0BAKZBVaNG6ipfAUO6iKZCBtyT9YXt0CBHUZCR3wKZBN8rBB5JHHrBhAy1q5QqRMSoVl71ZBzZCELa4tYMJgMgY7YP51KpgxxBZBKLcZBJQBYpIqMyVD2XZBn9kLzJTd6hLfBlZAGfvzSKgG2g6gDgYOLPZBS9TxBxCEFhHZAmaPsVVipY',
-  appSecret: 'bd48b3ead8fcaa9d4e08200ecca3b5cc',
-});
+// const bot = new MessengerBot({
+//   accessToken: 'EAAYzaWcruX0BAKZBVaNG6ipfAUO6iKZCBtyT9YXt0CBHUZCR3wKZBN8rBB5JHHrBhAy1q5QqRMSoVl71ZBzZCELa4tYMJgMgY7YP51KpgxxBZBKLcZBJQBYpIqMyVD2XZBn9kLzJTd6hLfBlZAGfvzSKgG2g6gDgYOLPZBS9TxBxCEFhHZAmaPsVVipY',
+//   appSecret: 'bd48b3ead8fcaa9d4e08200ecca3b5cc',
+// });
 
-bot.onEvent(async context => {
-  await context.sendText('Hello World');
-});
+// bot.onEvent(async context => {
+//   await context.sendText('Hello World');
+// });
 
-const server = createServer(bot);
+// const server = createServer(bot);
 
-server.listen(5000, () => {
-  console.log('server is running on 5000 port...');
-});
+// server.listen(5000, () => {
+//   console.log('server is running on 5000 port...');
+// });
+
+app.post('/webhook/', function (req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+		}
+	}
+	res.sendStatus(200)
+})
+
+const token = "EAAYzaWcruX0BACNZCdWoodwZAqLaNSJJPhYHhRTtuWVZCQJO8GkysO4QmEZAkX6SBN9ZC14MzK1cFPZBO3ztS5rd7NuqCutHwguZALXYMnwM6I2wvMsRh8djIZCIqxmiUNZArWNxkFon1hIvMGspzHPY8e257ApXo2FfW7mbGyZBdUBLCtHtsOeEfs"
+
+function sendTextMessage(sender, text) {
+	let messageData = { text:text }
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+	json: {
+			recipient: {id:sender},
+		message: messageData,
+	}
+}, function(error, response, body) {
+	if (error) {
+			console.log('Error sending messages: ', error)
+	} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
